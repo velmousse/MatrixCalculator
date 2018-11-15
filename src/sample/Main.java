90fcb466bf764519231727d97678c2c1da0adac9
Velmousse <supravelmousse@gmail.com>
2018-11-15 10:01:09 -0500

    Résolution et taille des matrices changées

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
         primaryStage.setTitle("Calculateur de matrices");
-        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.setScene(new Scene(root, 650, 650));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
